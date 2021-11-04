e99b3fb4e1aab294488896b205a67df3fbf869e1
ChrisWestesson <chris.westesson@gmail.com>
2021-11-04 13:47:44 +0100

    ny text

--- piaxmergit/ContentView.swift
+++ piaxmergit/ContentView.swift
@@ -11,11 +11,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hejsan världen!!!!!")
-            .foregroundColor(Color.white)
-            .padding()
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hejsan världen!!!!!")
+                .foregroundColor(Color.white)
+                .padding()
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
             .cornerRadius(/*@START_MENU_TOKEN@*/22.0/*@END_MENU_TOKEN@*/)
+            
+            Text("test").padding()
+        }
+        
+        
     }
 }
 
